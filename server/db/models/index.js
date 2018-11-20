4294458b81d116b887de90ad50cb8e695302dced
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-20 16:48:05 -0500

    table relations added

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,4 +2,14 @@ const User = require('./user');
 const Position = require('./position');
 const Transaction = require('./transaction');
 
+User.belongsToMany(Transaction, {
+  through: { model: 'UserTransaction', unique: false },
+});
+Transaction.belongsToMany(User, {
+  through: { model: 'UserTransaction', unique: false },
+});
+
+Position.belongsTo(User);
+User.hasMany(Position);
+
 module.exports = { User, Position, Transaction };
