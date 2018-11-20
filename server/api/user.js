132465a131c5f031e7adfbdee8144e0b51b895d5
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-20 17:32:05 -0500

    created route for portfolio

--- server/api/user.js
+++ server/api/user.js
@@ -5,10 +5,20 @@ module.exports = router;
 router.get('/:id', async (req, res, next) => {
   try {
     const id = req.params.id;
-    const user = await User.findById(id, {
-      include: [{ all: true }],
-    });
+    const user = await User.findById(id);
     res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id/portfolio', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id, {
+      include: [{ model: Position }],
+    });
+    res.json(user.positions);
   } catch (err) {
     next(err);
   }
