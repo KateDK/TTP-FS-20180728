38d8e877015c19959f6abc2771026b7120b001ca
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-24 16:56:32 -0500

    created Navbar component and rendered to screen. Removed makeReactApp header element

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import {connect} from 'react-redux'
+// import {Link} from 'react-router-dom'
+// import {logout} from './store'
+
+const Navbar = () => <h1>Navbar</h1>;
+
+export default Navbar;
