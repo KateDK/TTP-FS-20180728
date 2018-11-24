8e89eea594b47bd90760e719993164c21601ac72
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-24 16:28:37 -0500

    removed redundant code from api/index

--- server/api/index.js
+++ server/api/index.js
@@ -4,16 +4,6 @@ module.exports = router;
 router.use('/user', require('./user'));
 router.use('/auth', require('./auth'));
 
-router.get('/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-router.post('/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
-
 router.use((req, res, next) => {
   const error = new Error('Not Found');
   error.status = 404;
