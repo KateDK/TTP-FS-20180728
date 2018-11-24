04262095f74eabf876aaad4a7d7a525adcedc489
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-24 16:42:39 -0500

    added the reactSignup component and rendered to the screen

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import ReactSignupLoginComponent from 'react-signup-login-component';
 
-const AuthForm = () => <h1>This is the login/signup page</h1>;
+const AuthForm = () => {
+  const signupWasClickedCallback = data => {
+    console.log(data);
+    alert('Signup callback, see log on the console to see the data.');
+  };
+  const loginWasClickedCallback = data => {
+    console.log(data);
+    alert('Login callback, see log on the console to see the data.');
+  };
+  const recoverPasswordWasClickedCallback = data => {
+    console.log(data);
+    alert('Recover password callback, see log on the console to see the data.');
+  };
+  return (
+    <div>
+      <ReactSignupLoginComponent
+        usernameCustomLabel="Email"
+        title="Signup/Signin"
+        handleSignup={signupWasClickedCallback}
+        handleLogin={loginWasClickedCallback}
+        handleRecoverPassword={recoverPasswordWasClickedCallback}
+        styles={{
+          recoverPassword: {
+            wrapper: { backgroundColor: 'yellow' },
+            inputWrapper: { backgroundColor: 'AliceBlue' },
+            buttonsWrapper: { backgroundColor: 'Aqua' },
+            input: { backgroundColor: 'LavenderBlush' },
+            button: { backgroundColor: 'LightCoral' },
+          },
+        }}
+      />
+    </div>
+  );
+};
 
 export default AuthForm;
