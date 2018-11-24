14a7d48cb514e380df0bb3ccd97b5dae2aa5386e
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-24 15:47:19 -0500

    added boilerplate for user in store

---
+++ client/src/store/user.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import history from '../history';
+
+/**
+ * ACTION TYPES
+ */
+const GET_USER = 'GET_USER';
+const REMOVE_USER = 'REMOVE_USER';
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {};
+
+/**
+ * ACTION CREATORS
+ */
+
+/**
+ * THUNK CREATORS
+ */
+
+/**
+ * REDUCER
+ */
