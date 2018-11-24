7520e4ce54c27a03bee16793556cc6e127e75879
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-24 17:16:48 -0500

    connected app to store

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-// import {connect} from 'react-redux'
-// import {Link} from 'react-router-dom'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 // import {logout} from './store'
 
-const Navbar = () => <h1>Navbar</h1>;
+const Navbar = () => (
+  <div>
+    <nav className="navbar">
+      <div className="collapse navbar-collapse" id="navbarLinks">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">Sign Up / Logg In</li>
+          <li className="nav-item">Logout</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
 
 export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -9,12 +9,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  // <Provider store={store}>
-  //   <Router history={history}>
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-  // {/* </Router>
-  // </Provider> */}
 );
 
 // If you want your app to work offline and load faster, you can change
