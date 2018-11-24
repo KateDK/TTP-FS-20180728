da71549e0e53d0c72fda4e1bb1f23b9566e586cd
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-24 15:54:03 -0500

    updated import of create Logger to new version due to error in console

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />, document.getElementById('root')
-    </Router>
-  </Provider>
+  // <Provider store={store}>
+  //   <Router history={history}>
+  <App />,
+  document.getElementById('root')
+  // {/* </Router>
+  // </Provider> */}
 );
 
 // If you want your app to work offline and load faster, you can change
--- client/src/store/user.js
+++ client/src/store/user.js
@@ -23,3 +23,13 @@ const defaultUser = {};
 /**
  * REDUCER
  */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user;
+    case REMOVE_USER:
+      return defaultUser;
+    default:
+      return state;
+  }
+}
