3ba417803740e25cfe82a4e07a574c6358d93086
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-25 20:43:31 -0500

    added form to buyForm component with two way binding

--- client/src/components/BuyForm.js
+++ client/src/components/BuyForm.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BuyForm = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>{props.userBalance}</h1>
-      <h1>Form</h1>
-    </div>
-  );
-};
+class BuyForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticker: '',
+      quantity: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { userBalance } = this.props;
+    console.log(this.state);
+    return (
+      <div>
+        <h1>{userBalance}</h1>
+        <form>
+          <label>Ticker</label>
+          <input
+            type="text"
+            name="ticker"
+            value={this.state.ticker}
+            onChange={this.handleChange}
+          />
+          <label>Qty</label>
+          <input
+            type="number"
+            name="quantity"
+            value={this.state.quantity}
+            onChange={this.handleChange}
+          />
+          <button type="submit">BUY</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default BuyForm;
