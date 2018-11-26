fd26bae4ccf27a28cc2f4b63bb2d9c7243d2a7b8
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-26 17:34:51 -0500

    remove redundant code from Portfolio component

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -5,10 +5,6 @@ import { fetchPositions } from '../store';
 import PositionCard from './PositionCard';
 
 class Portfolio extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.props.fetchPositions();
   }
