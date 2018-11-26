3ede6cbe39ca1742896f96273c6a159bc340fd3c
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-26 15:34:49 -0500

    navbar will now not show on login page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,35 +3,27 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
 
-const Navbar = ({ isLoggedIn, handleClick }) => (
-  <div>
-    <nav className="navbar">
-      <div className="collapse navbar-collapse" id="navbarLinks">
-        <ul className="navbar-nav mr-auto">
-          {isLoggedIn ? (
-            <span>
-              <li className="nav-item">
-                <Link to="/portfolio">Portfolio</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/transactions">Transactions</Link>
-              </li>
-              <li className="nav-item">
-                <a href="#" onClick={handleClick} className="nav-link">
-                  Logout
-                </a>
-              </li>
-            </span>
-          ) : null
-          // <li className="nav-item">
-          //   <Link to="/">Sign Up / Logg In</Link>
-          // </li>
-          }
+const Navbar = ({ isLoggedIn, handleClick }) =>
+  isLoggedIn ? (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav">
+          <span className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/transactions">Transactions</Link>
+            </li>
+
+            <a href="#" onClick={handleClick} className="nav-item nav-link">
+              Logout
+            </a>
+          </span>
         </ul>
-      </div>
-    </nav>
-  </div>
-);
+      </nav>
+    </div>
+  ) : null;
 
 const mapState = state => {
   return {
