8135313d1eae9013c02afefdd58084da33cd214f
Kate Dubitski <kate.dubitski@gmail.com>
2018-11-27 11:43:22 -0500

    now displaying small arrows to show the trend

--- client/src/components/PositionCard.js
+++ client/src/components/PositionCard.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const PositionCard = props => {
   const { tickerSymbol, numShares, totalStockVal, trend } = props.position;
-
+  let arrow = '▶';
+  if (trend === 'up') {
+    arrow = '▲';
+  }
+  if (trend === 'down') {
+    arrow = '▼';
+  }
   return (
     <div className="positionCard">
       <span>
@@ -10,7 +16,7 @@ const PositionCard = props => {
       </span>
       <span className="positionCardItem">
         <span className={trend}>${Number(totalStockVal).toFixed(2)} </span>
-        <span>{trend} </span>
+        <span className={trend}>{arrow} </span>
       </span>
     </div>
   );
